8d58381f6e7a08fb7cb40474644fead9ae8823e4
Abhishek Yelisetti <abhishek.yelisetti@zemosolabs.com>
2021-05-12 19:05:49 +0530

    Completed Week 2 Assignment

---
+++ Week 2/eventplan.sql
@@ -0,0 +1,11 @@
+CREATE TABLE EventPlan (
+  planNo   CHAR(8)  NOT NULL,
+  evntNo   CHAR(8)  NOT NULL,
+  empNo    CHAR(8),
+  workDate DATE     NOT NULL,
+  notes    TEXT,
+  activity CHAR(10) NOT NULL,
+  CONSTRAINT PK_EventPlan PRIMARY KEY (planNo),
+  CONSTRAINT FK_EventRequest FOREIGN KEY (evntNo) REFERENCES EventRequest (evntNo),
+  CONSTRAINT FK_Employee FOREIGN KEY (empNo) REFERENCES Employee (empNo)
+);
---
+++ Week 2/location.sql
@@ -0,0 +1,7 @@
+CREATE TABLE Location (
+  locNo   CHAR(8)     NOT NULL,
+  facNo   CHAR(8)     NOT NULL,
+  locName VARCHAR(30) NOT NULL,
+  CONSTRAINT LocationPK PRIMARY KEY (locNo),
+  CONSTRAINT FacilityFK FOREIGN KEY (facNo) REFERENCES Facility (facNo)
+);
